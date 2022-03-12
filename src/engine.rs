95e49ab247567563ded8e32db7d5877f9aaf62c9
Paolo <str237@gmail.com>
2022-03-12 13:53:25 +0100

    WIP ingest csv

--- src/engine.rs
+++ src/engine.rs
@@ -1,13 +1,35 @@
 use crate::amount::Amount;
+use crate::error::DynResult;
+use crate::transaction::Transaction;
+use std::collections::HashMap;
 
-struct EngineStatus {
-  balances: Vec<ClientBalance>
+type ClientId = u16;
+
+#[derive(Debug, Default, Clone)]
+struct Engine {
+  balances: HashMap<ClientId, ClientBalance>,
 }
 
+#[derive(Debug, Default, Clone, Copy)]
 struct ClientBalance {
-  client: u16,
+  client: ClientId,
   available: Amount,
   held: Amount,
   total: Amount,
   locked: bool,
+}
+
+impl Engine {
+  fn ingest_csv(&mut self, filename: &str) -> DynResult<()> {
+    csv::ReaderBuilder::new()
+      .trim(csv::Trim::All)
+      .from_path(filename)?
+      .deserialize()
+      .filter(|res| res.is_ok())
+      .for_each(|res| {
+        let transaction: Transaction = res.unwrap();
+        println!("{:?}", &transaction);
+      });
+    Ok(())
+  }
 }
