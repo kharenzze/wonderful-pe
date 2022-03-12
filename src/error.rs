526a8e83b4c03d3615e9bc05bd911f9e9b712b37
Paolo <str237@gmail.com>
2022-03-12 13:47:33 +0100

    DynResult

--- src/error.rs
+++ src/error.rs
@@ -3,3 +3,5 @@ use thiserror::Error;
 #[derive(Debug, Error, PartialEq, Eq)]
 #[error("Could not parse string")]
 pub struct ParsingError();
+
+pub type DynResult<T> = Result<T, Box<dyn std::error::Error>>;
