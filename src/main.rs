cb355686f926a1ba968a8ec7dc22f180f1ddb661
Paolo <str237@gmail.com>
2022-03-12 14:05:32 +0100

    Basic main program

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use wonderful_pe::engine::Engine;
+
 fn main() {
-  println!("Hello, world!");
-  let engine: Engine = Default::default();
+  let mut args = std::env::args();
+  if args.len() != 2 {
+    panic!("There must be an argument, which is the csv input path.")
+  }
+  let path = args.nth(1).unwrap();
+  let mut engine: Engine = Default::default();
+  engine.ingest_csv(&path).expect("Error processing input file");
 }
