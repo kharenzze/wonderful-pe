985dd6e4899ce2577ac3b000084427e6822521a5
Paolo Raez <str237@gmail.com>
2022-03-15 23:14:21 +0100

    WIP Handling transactions

--- src/engine.rs
+++ src/engine.rs
@@ -1,6 +1,6 @@
 use crate::amount::Amount;
 use crate::error::DynResult;
-use crate::transaction::Transaction;
+use crate::transaction::{Transaction, TransactionType};
 use std::collections::HashMap;
 
 type ClientId = u16;
@@ -19,6 +19,18 @@ struct ClientBalance {
   locked: bool,
 }
 
+impl ClientBalance {
+  fn new(client: ClientId) -> Self {
+    Self {
+      client,
+      available: Default::default(),
+      held: Default::default(),
+      total: Default::default(),
+      locked: false,
+    }
+  }
+}
+
 impl Engine {
   pub fn ingest_csv(&mut self, filename: &str) -> DynResult<()> {
     csv::ReaderBuilder::new()
@@ -31,5 +43,37 @@ impl Engine {
         println!("{:?}", &transaction);
       });
     Ok(())
+  }
+
+  fn apply_transaction(&mut self, transaction: &Transaction) -> DynResult<()> {
+    let mut balance = self.get_or_create_mut_balance(transaction.client);
+    match transaction.type_ {
+      TransactionType::Deposit => {
+        balance.total += transaction.amount;
+        balance.available += transaction.amount;
+      },
+      TransactionType::Withdrawal => {
+        balance.available = balance.available.checked_sub(transaction.amount).ok_or("Not enough available")?;
+        balance.total = balance.total.checked_sub(transaction.amount).unwrap();
+      },
+      TransactionType::Dispute => {
+        unimplemented!();
+      },
+      TransactionType::Resolve => {
+        unimplemented!();
+      },
+      TransactionType::Chargeback => {
+        unimplemented!();
+      },
+      _ => unimplemented!()
+    }
+    Ok(())
+  }
+
+  fn get_or_create_mut_balance(&mut self, client: ClientId) -> &mut ClientBalance {
+    if self.balances.get(&client).is_none() {
+      self.balances.insert(client, ClientBalance::new(client));
+    }
+    self.balances.get_mut(&client).unwrap()
   }
 }
