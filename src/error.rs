dd2c74d33c51aa33ff4607c57cbd099ace7350bb
Paolo Raez <str237@gmail.com>
2022-03-15 23:57:07 +0100

    New error

--- src/error.rs
+++ src/error.rs
@@ -4,4 +4,10 @@ use thiserror::Error;
 #[error("Could not parse string")]
 pub struct ParsingError();
 
+#[derive(Debug, Error, PartialEq, Eq)]
+pub enum TransactionProcessingError {
+  #[error("Duplicated. Tx already known")]
+  Duplicated
+} 
+
 pub type DynResult<T> = Result<T, Box<dyn std::error::Error>>;
