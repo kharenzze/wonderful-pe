136b5a4d5c6f24f20176b79458e99b704ce31652
Paolo Raez <str237@gmail.com>
2022-03-16 15:10:54 +0100

    More errors

--- src/error.rs
+++ src/error.rs
@@ -12,10 +12,22 @@ pub enum TransactionProcessingError {
   TargetAccountLocked,
   #[error("Available money is less than withdraw amount")]
   NotEnoughAvailable,
+  #[error("Held money is less than expected")]
+  NotEnoughHeld,
+  #[error("Total money is less than expected")]
+  NotEnoughTotal,
   #[error("Target transaction does not exist")]
   MissingTargetTransaction,
-  #[error("TransactionAlreadyDisputed")]
-  AlreadyDisputed
-} 
+  #[error("Transaction already disputed")]
+  AlreadyDisputed,
+  #[error("Can only dispute Deposit transactions")]
+  DisputingWrongTransactionType,
+  #[error("Missing transaction amount")]
+  MissingTransactionAmount,
+  #[error("Client does not match")]
+  ClientDoesNotMatch,
+  #[error("Transaction is not being disputed")]
+  NotDisputed,
+}
 
 pub type DynResult<T> = Result<T, Box<dyn std::error::Error>>;
